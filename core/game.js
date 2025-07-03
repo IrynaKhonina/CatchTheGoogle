b4611b4f69f8f43339fe308dd17fb0f4b72100f7
IrynaKhonina <irinahonina@yahoo.com>
2025-07-03 16:09:12 +0300

    add makeJumpGoogle

--- core/game.js
+++ core/game.js
@@ -3,7 +3,8 @@ import {GameStatuses} from "./game-statuses";
 export class Game {
     #status = GameStatuses.SETTINGS
     #googlePosition = null
-    #NumberUtility
+
+    #NumberUtility //=new ShogunNumberUtility()
 
     constructor(somethingSimilarToNumberUtility) {
         this.#NumberUtility = somethingSimilarToNumberUtility
@@ -16,17 +17,45 @@ export class Game {
         },
         googleJumpInterval:1000
     }
+    /**
+     * Sets the Google jump interval (in milliseconds).
+     * @param {number} newValue - The new jump interval value (must be a positive number).
+     * @throws {TypeError} If the provided value is not a number.
+     * @throws {RangeError} If the provided value is not a positive number.
+     * @returns {void}
+     */
+    set googleJumpInterval(newValue) {
+        if (typeof newValue !== 'number') {
+            throw new TypeError("Argument must be a numbers");
+        }
+        if (newValue <=0){
+            throw new TypeError("Interval must be a numbers");
+        }
+        this.#settings.googleJumpInterval = newValue;
+    }
 
     start() {
         if (this.#status !== GameStatuses.SETTINGS) {
             throw new Error("Game must be in Settings before Start");
         }
         this.#status = GameStatuses.IN_PROGRESS;
-        this.#googlePosition ={
-            x:this.#NumberUtility.getRandomIntegerNumber(0, this.#settings.gridSize.columnCount),
-            y:this.#NumberUtility.getRandomIntegerNumber(0, this.#settings.gridSize.rowCount),
-        }
+          this.#makeGoogleJump()
 
+        setInterval(()=>{
+            this.#makeGoogleJump()
+        }, this.#settings.googleJumpInterval);
+
+    }
+    #makeGoogleJump(){
+        const newPosition ={
+            x:this.#NumberUtility.getRandomIntegerNumber(0, this.#settings.gridSize.columnCount -1),
+            y:this.#NumberUtility.getRandomIntegerNumber(0, this.#settings.gridSize.rowCount -1 ),
+        }
+        if(newPosition.x === this.#googlePosition?.x && newPosition.y === this.#googlePosition.y){
+            this.#makeGoogleJump()
+            return
+        }
+        this.#googlePosition = newPosition;
     }
 
     getStatus() {
--- core/game.test.js
+++ core/game.test.js
@@ -20,13 +20,37 @@ it('game should be created and return status ', async () => {
 
 it('google should be in the Grid after start', async () => {
     const numberUtil = new ShogunNumberUtility()
-    const game = new Game(numberUtil)
-    // expect(game.googlePosition).toBeNull()
-    expect(game.getGooglePosition()).toBeNull()
-    await game.start()
-    // expect(game.googlePosition.x).toBeLessThan(game.gridSize.columnCount)
-    expect(game.getGooglePosition().x).toBeLessThan(game.gridSize.columnCount)
-    expect(game.getGooglePosition().x).toBeGreaterThanOrEqual(0)
-    expect(game.getGooglePosition().y).toBeLessThan(game.gridSize.rowCount)
-    expect(game.getGooglePosition().y).toBeGreaterThanOrEqual(0)
+
+    for (let i = 0; i < 1000; i++) {
+        const game = new Game(numberUtil)
+        // expect(game.googlePosition).toBeNull()
+        expect(game.getGooglePosition()).toBeNull()
+        await game.start()
+        // expect(game.googlePosition.x).toBeLessThan(game.gridSize.columnCount)
+        expect(game.getGooglePosition().x).toBeLessThan(game.gridSize.columnCount)
+        expect(game.getGooglePosition().x).toBeGreaterThanOrEqual(0)
+        expect(game.getGooglePosition().y).toBeLessThan(game.gridSize.rowCount)
+        expect(game.getGooglePosition().y).toBeGreaterThanOrEqual(0)
+    }
+
+
 })
+
+
+it('google should be in the Grid but in position after jump', async () => {
+    const numberUtil = new ShogunNumberUtility()
+    const game = new Game(numberUtil)
+    game.googleJumpInterval = 1 //ms
+    await game.start() // jump - web API /browser 10
+
+    for (let i = 0; i < 100; i++) {
+        const prevGooglePosition = game.googlePosition;
+        await delay(1) // await -> webAPI/browser 10 // after 10 ms: macrotasks: [jump, game.googlePosition;
+        const currentGooglePosition = game.getGooglePosition()
+        expect(prevGooglePosition).not.toEqual(currentGooglePosition)
+    }
+
+
+})
+// промисификация setTimeout
+const delay = (ms) => new Promise(res => setTimeout(res, ms))
